569148434e763f89a35bc2c0b39b362da3e6d90b
Anirban Bera <anirban.bera.89@gmail.com>
2019-06-01 20:06:02 +0530

    thumbnail route added

--- app.js
+++ app.js
@@ -44,6 +44,27 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/thumbnail', function (req, res) {
+  // create the screenshot
+  webshot(req.query.url, 'output-thumbnail.png', optionsMobile, function (err) {
+    if (!err) {
+      console.log('screenshot taken!');
+
+      res.status(200).json({
+        'id': 'some image',
+        'imgEncoded': base64_encode('output-thumbnail.png')
+      });
+    }
+  });
+
+});
+
+var base64_encode = function (file) {
+  // read binary data
+  var bitmap = fs.readFileSync(file);
+  // convert binary data to base64 encoded string
+  return new Buffer(bitmap).toString('base64');
+}
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/user.js
+++ routes/user.js
@@ -4,22 +4,5 @@
  */
 
 exports.list = function(req, res){
-  // res.send("respond with a resource");
-  webshot(req.query.url, 'output-thumbnail.png', optionsMobile, function (err) {
-    if (!err) {
-      console.log('screenshot taken!');
-
-      res.status(200).json({
-        'id': 'some image',
-        'imgEncoded': base64_encode('output-thumbnail.png')
-      });
-    }
-  });
+  res.send("From express");
 };
-
-var base64_encode = function (file) {
-  // read binary data
-  var bitmap = fs.readFileSync(file);
-  // convert binary data to base64 encoded string
-  return new Buffer(bitmap).toString('base64');
-}
